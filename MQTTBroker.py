ce4a035526435afa9e62037e3e0fd820865a327f
Tanmoy Dutta <duttatanmoy834@gmail.com>
2023-07-18 10:45:23 +0530

    First Commit

---
+++ MQTTBroker.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import keyboard
+import json
+
+# MQTT broker details
+data = None
+topics = ["/traffic/slave_feedback","/traffic/master_feedback"]
+broker = "traffic-controller.cloud.shiftr.io"
+port = 1883
+username = "traffic-controller"
+password = "sZG1eCQPPOS7nmBg"
+client_id = "GUIClient"
+
+# Callback functions
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT broker")
+    else:
+        print("Failed to connect, return code: " + str(rc))
+
+def on_publish(client, userdata, mid):
+    print("Message published")
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed to topic")
+
+# Create MQTT client and set callbacks
+client = mqtt.Client(client_id)
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
